0aa7fb57f966077fc2df23af31d2b41d36dc771b
Diana Agapkina <agapkinadiana@users.noreply.github.com>
2020-10-07 15:00:43 +0300

    init: set up project

---
+++ Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Diana Agapkina on 10/7/20.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
